14b64299d60b4fabd634439203275f17a69e1b39
z4n0 <luca.zanotto@mail.polimi.it>
2021-06-07 00:04:37 +0200

    adjacency list done, starting implementing dijkstra

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 #include <string.h>
 #define MAX_COMMANDLENGHT 22
 #define MAX_FIRSTCOMMANDLENGHT 6
+#define INFINITY 999
+#define TEMP 0
+#define PERM 1
+#define NIL 1
 
+int numberOfVerteces;
+int lunghezzaClassifica;
 //struct Vertex;
 //struct Edge;
 
@@ -15,9 +21,11 @@ typedef struct edge_node{
 
 typedef struct vertex_node{
     int vertexIndex;
+    int status;
     //struct vertex* nextVertex; //prossimo vertice nell ARRAY dei vertici
     struct edge_node* firstEdge; //primo edge nella tab di adiacenza
 }Vertex;
+
 
 
 void InitializeVerteces(Vertex *vertices, int numberOfVerteces){
@@ -34,15 +42,47 @@ void InitializeVerteces(Vertex *vertices, int numberOfVerteces){
 void addEdge(Vertex *vertices, int edgeWeight, int startIndex, int destinationIndex){
     Edge *newEdge = malloc(sizeof (Edge)); //il nuovo edge da inserire
 
-    newEdge->destinationVertex = (struct Vertex *) &vertices[destinationIndex];
+    newEdge->destinationVertex = &vertices[destinationIndex];
     newEdge->edgeWeight = edgeWeight;
     newEdge->nextEdge = vertices[startIndex].firstEdge;
 
     vertices[startIndex].firstEdge = newEdge;
+}
 
-//    if(vertices[startIndex].firstEdge == NULL){
-//        vertices[startIndex].firstEdge =
-//    }
+void dijkstra(Vertex* vertices, int* predecessorIndex, int* distance){
+    int current;
+
+    for(int i=0; i<numberOfVerteces; i++){
+        distance[i]=INFINITY;
+        //status[i]=TEMP;
+        predecessorIndex[i]= NIL;
+    }
+
+    distance[0]=0;
+
+    while (1){ //continua fino a che min_temp non riesce a trovare altri nodi
+        current = min_temp(vertices,distance);//cerca il nodo adj con minor distanza possibile in statto temporaneo restituisce il suo indice
+        if(current==NIL){
+            return;
+        }
+        //TODO remeber to make status[current]=PERM;
+
+    }
+}
+
+//returns the temporary vertex with minim value of distance, returns NIL if no temporary vertex is left or
+//all temporary vertices left have pathlenght infinity
+int min_temp(Vertex* vertices,int* distance){
+    int i;
+    int min = INFINITY;
+    int k = NIL;
+    for(i=0; i<numberOfVerteces;i++){
+        if(vertices[i].status==TEMP && distance[i]<min){
+            min=distance[i];
+            k=i;
+        }
+    }
+    return k;
 }
 
 
@@ -58,8 +98,6 @@ int main() {
 
 
     //ARRAY di char che conterrano il primo comando
-    int numberOfVerteces;
-    int lunghezzaClassifica;
     char numberContainer[MAX_FIRSTCOMMANDLENGHT];
     char firstcommand[10];
     char inputContainer[22];
@@ -91,35 +129,41 @@ int main() {
 
     //inizializzazione ARRAY dei vertici
     Vertex vertices[numberOfVerteces];
-    InitializeVerteces(vertices,numberOfVerteces);
+    InitializeVerteces(vertices, numberOfVerteces);
 
     //lettura comandi  2 casi possibili
     //1.AggiungiGrafo--> leggi Matrice --> riempi lista di adiacenza
     //2.Topk--> stampa classifica
 
-    while(fgets(inputContainer, MAX_COMMANDLENGHT, fp) != NULL){ //fino a che si puo leggere
+    while (fgets(inputContainer, MAX_COMMANDLENGHT, fp) != NULL) { //fino a che si puo leggere
 
-
-        if(strcmp(inputContainer,"AggiungiGrafo\n")==0){
+        if (strcmp(inputContainer, "AggiungiGrafo\n") == 0) {
             memset(inputContainer, 0, MAX_COMMANDLENGHT); //svuoto inputContainer
-
+//            j=0;
             //Se il comando è di aggiungi grafo -->leggi la matrice nxn
-            for(i=0; i<numberOfVerteces; i++){
+            for (i = 0; i < numberOfVerteces; i++) {
                 fgets(inputContainer, MAX_COMMANDLENGHT, fp);    //leggo riga matrice
                 strtok(inputContainer, "\n");   //elimino il\n
                 //separo i numeri della riga
-                char* edgeWeight = strtok(inputContainer, " ");
+                char *edgeWeightToken = strtok(inputContainer, " ");
+                j = 0; //indice delle colonne
 
-                addEdge(vertices, edgeWeight, i);
 //              walk through other number of the Line
-                while(edgeWeight != NULL ) {
-                    //printf(" %s\n", edgeWeight );
-                    edgeWeight = strtok(NULL, " ");
+                while (edgeWeightToken != NULL) {
+                    //printf(" %s\n", edgeWeightToken );
+                    int edgeWeight = (int) strtol(edgeWeightToken, NULL, 10);//converto il token a int
+                    addEdge(vertices, edgeWeight, i, j++);
+                    edgeWeightToken = strtok(NULL, " ");
                 }
             }
-        }else if(strcmp(inputContainer,"TopK")==0){
+            int predecessor[numberOfVerteces]; //array che contiene l'ID del hop precedente al nodo i
+            int distance[numberOfVerteces]; //array che contiene la distanza del nodo i dal nodo zero
+            dijkstra(vertices,predecessor,distance);
+            //TODO comando che chiama djsktra e inserisce numero cammini minimi e id del grafo in una min heap
+        } else if (strcmp(inputContainer, "TopK") == 0) {
             //TODO TOPK command
+            //TODO fa semplicemente un estrazione dalla min heap tante volte qualta è la lunghezza della classifica
         }
     }
     return 0;
-
+}
